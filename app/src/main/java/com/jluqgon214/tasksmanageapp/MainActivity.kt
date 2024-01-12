8eaa65c334cd5f6dfcf296c546a60c5d6ddd1141
José Manuel Luque <jluqgon214@g.educaand.es>
2024-01-12 19:43:15 +0100

    Parte 1 Finalizada

---
+++ app/src/main/java/com/jluqgon214/tasksmanageapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.jluqgon214.tasksmanageapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.jluqgon214.tasksmanageapp.addtasks.ui.TasksScreen
+import com.jluqgon214.tasksmanageapp.addtasks.ui.TasksViewModel
+import com.jluqgon214.tasksmanageapp.ui.theme.TasksManageAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    private val tasksViewModel: TasksViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TasksManageAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TasksScreen(tasksViewModel)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/jluqgon214/tasksmanageapp/TasksManageApp.kt
@@ -0,0 +1,7 @@
+package com.jluqgon214.tasksmanageapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TasksManageApp : Application()
